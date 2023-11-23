0b6833d3cb132e85f9384d20e58da86386e4334d
Zara Martín <zmartand@myuax.es>
2023-11-23 19:10:58 +0100

    Bibliotecario

--- src/main/java/com/example/bibliotecazara1/entidades/Bibliotecario.java
+++ src/main/java/com/example/bibliotecazara1/entidades/Bibliotecario.java
@@ -1,4 +1,49 @@
 package com.example.bibliotecazara1.entidades;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Bibliotecario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String identificacion;
+    private String contacto;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
 }
+
