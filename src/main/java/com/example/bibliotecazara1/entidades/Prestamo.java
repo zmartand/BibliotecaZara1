b6e7c7e658ac78919120c4e2ff9850111b00b176
Zara Martín <zmartand@myuax.es>
2023-11-23 19:52:20 +0100

    Prestamo

--- src/main/java/com/example/bibliotecazara1/entidades/Prestamo.java
+++ src/main/java/com/example/bibliotecazara1/entidades/Prestamo.java
@@ -1,6 +1,7 @@
 package com.example.bibliotecazara1.entidades;
 
 import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 public class Prestamo {
@@ -12,4 +13,52 @@ public class Prestamo {
     @JoinColumn(name = "lector_id")
     private Lector lector;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "libro_id")
+    private Libro libro;
+
+    private LocalDate fechaPrestamo;
+    private LocalDate fechaDevolucion;
+
+    // Getters y setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Lector getLector() {
+        return lector;
+    }
+
+    public void setLector(Lector lector) {
+        this.lector = lector;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    public LocalDate getFechaPrestamo() {
+        return fechaPrestamo;
+    }
+
+    public void setFechaPrestamo(LocalDate fechaPrestamo) {
+        this.fechaPrestamo = fechaPrestamo;
+    }
+
+    public LocalDate getFechaDevolucion() {
+        return fechaDevolucion;
+    }
+
+    public void setFechaDevolucion(LocalDate fechaDevolucion) {
+        this.fechaDevolucion = fechaDevolucion;
+    }
 }
