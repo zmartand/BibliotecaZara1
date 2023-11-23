af5cb571833187e18db11948ba772aa9ddde1f5a
Zara Martín <zmartand@myuax.es>
2023-11-23 18:55:53 +0100

    Lector hecho

--- src/main/java/com/example/bibliotecazara1/entidades/Lector.java
+++ src/main/java/com/example/bibliotecazara1/entidades/Lector.java
@@ -1,4 +1,21 @@
 package com.example.bibliotecazara1.entidades;
 
+import jakarta.persistence.*;
+import java.util.Set;
+
+@Entity
 public class Lector {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String identificacion;
+    private String contacto;
+
+    // Relación con Prestamo
+    @OneToMany(mappedBy = "lector", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Set<Prestamo> prestamos;
+
+    // Getters y setters ...
 }
