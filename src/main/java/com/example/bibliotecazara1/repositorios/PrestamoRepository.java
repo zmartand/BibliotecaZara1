c219b661c0d9519a41e67b0ea1648f5ca898238e
Zara Martín <zmartand@myuax.es>
2023-11-23 19:12:29 +0100

    Prestamo interfaz repositorio

--- src/main/java/com/example/bibliotecazara1/repositorios/PrestamoRepository.java
+++ src/main/java/com/example/bibliotecazara1/repositorios/PrestamoRepository.java
@@ -2,8 +2,12 @@ package com.example.bibliotecazara1.repositorios;
 
 
 import com.example.bibliotecazara1.entidades.Prestamo;
+import com.example.bibliotecazara1.entidades.Lector;
+import com.example.bibliotecazara1.entidades.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PrestamoRepository extends JpaRepository<Prestamo, Long> {
-
+    List<Prestamo> findByLector(Lector lector);
+    List<Prestamo> findByLibro(Libro libro);
 }
