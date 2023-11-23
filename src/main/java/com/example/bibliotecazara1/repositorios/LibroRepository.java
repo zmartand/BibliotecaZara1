1dfa9e98610bd4963b4887b6e7cf94ae8aeb6377
Zara Martín <zmartand@myuax.es>
2023-11-23 19:08:49 +0100

    interfaz Libro

--- src/main/java/com/example/bibliotecazara1/repositorios/LibroRepository.java
+++ src/main/java/com/example/bibliotecazara1/repositorios/LibroRepository.java
@@ -2,6 +2,10 @@ package com.example.bibliotecazara1.repositorios;
 
 import com.example.bibliotecazara1.entidades.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface LibroRepository extends JpaRepository<Libro, Long> {
+    List<Libro> findByTituloContaining(String titulo);
+    List<Libro> findByAutor(String autor);
+    Libro findByIsbn(String isbn);
 }
