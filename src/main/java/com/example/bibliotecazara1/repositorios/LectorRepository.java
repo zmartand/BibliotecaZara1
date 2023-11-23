a19b19694ce256e079c35dee498547d097ce3884
Zara Martín <zmartand@myuax.es>
2023-11-23 19:11:34 +0100

    Lector interfaz

--- src/main/java/com/example/bibliotecazara1/repositorios/LectorRepository.java
+++ src/main/java/com/example/bibliotecazara1/repositorios/LectorRepository.java
@@ -2,7 +2,9 @@ package com.example.bibliotecazara1.repositorios;
 
 import com.example.bibliotecazara1.entidades.Lector;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface LectorRepository extends JpaRepository<Lector, Long> {
-
+    List<Lector> findByNombreContaining(String nombre);
+    Lector findByIdentificacion(String identificacion);
 }
