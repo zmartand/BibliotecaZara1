e41cf7a5ea1b14bf1c6a64fccb762642b15a7845
Zara Martín <zmartand@myuax.es>
2023-11-23 19:52:04 +0100

    Lector

--- src/main/java/com/example/bibliotecazara1/entidades/Lector.java
+++ src/main/java/com/example/bibliotecazara1/entidades/Lector.java
@@ -17,5 +17,43 @@ public class Lector {
     @OneToMany(mappedBy = "lector", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<Prestamo> prestamos;
 
-    // Getters y setters ...
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    public Set<Prestamo> getPrestamos() {
+        return prestamos;
+    }
+
+    public void setPrestamos(Set<Prestamo> prestamos) {
+        this.prestamos = prestamos;
+    }
 }
