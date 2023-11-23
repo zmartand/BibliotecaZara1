fa3d6d0a23263c557e87f0c72ff2aa71bfab4ae6
Zara Martín <zmartand@myuax.es>
2023-11-23 18:58:18 +0100

    Prestamo

--- src/main/java/com/example/bibliotecazara1/entidades/Prestamo.java
+++ src/main/java/com/example/bibliotecazara1/entidades/Prestamo.java
@@ -1,4 +1,15 @@
 package com.example.bibliotecazara1.entidades;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Prestamo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "lector_id")
+    private Lector lector;
+    
 }
