115c43b86262a3169bfaa7e0396fb721e48783d5
Zara Martín <zmartand@myuax.es>
2023-11-23 19:12:48 +0100

    Bibliotecario repositorio interfaz

--- src/main/java/com/example/bibliotecazara1/repositorios/BibliotecarioRepository.java
+++ src/main/java/com/example/bibliotecazara1/repositorios/BibliotecarioRepository.java
@@ -1,7 +1,8 @@
 package com.example.bibliotecazara1.repositorios;
 import com.example.bibliotecazara1.entidades.Bibliotecario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface BibliotecarioRepository extends JpaRepository<Bibliotecario, Long> {
-    
+    List<Bibliotecario> findByNombreContaining(String nombre);
 }
